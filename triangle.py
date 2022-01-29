23a1ca980af099f03b3abb0ba496c48c108002c3
Joe Bussard <jbussard@ucsc.edu>
2022-01-29 14:23:32 -0800

    Update triangle.py
    added support for custom susbtances.
    
    added support for emoji's - however, this currently works for computers that render emojis as taking up 2 spaces, because thats how zsh renders emojis on my machine.

--- triangle.py
+++ triangle.py
@@ -1,18 +1,25 @@
 #!/bin/python
 import sys # for command line arguments
+import emoji
 TRIANGLE_CHAR = "▲"
 
-def print_triangle(height, size):
+def print_triangle(height, size, char = TRIANGLE_CHAR):
+    char_size = len(char)
+    for x in char:
+      if emoji.is_emoji(x):
+          char_size += 1
     for level in range(height):                        # height of the n-force
         for x in range(size):                          # scale of each triangle
-            print(" " * (size * (height - level - 1)), # left margin for this entire level
-                    (" " * (size - x - 1) +            # left margin before the first ▲
-                    TRIANGLE_CHAR * (2*x + 1) +        # the ▲'s
-                    " " * (size - x)) * (level+1),     # right margin after the last ▲
+            print(" " * (size * char_size * (height - level - 1)), # left margin for this entire level
+                    (" " * ((size - x -1) * char_size) +            # left margin before the first ▲
+                    char * (2*x + 1) +                 # the ▲'s
+                    " " * (char_size * (size-x))) * (level + 1),     # right margin after the last ▲
                                                        # repeat for all triangles on this level
                     sep='')                            # all spacing is handled manually
 try:
-    print(f"printing a {int(sys.argv[1])}-force of scale {int(sys.argv[2])}")
-    print_triangle(int(sys.argv[1]), int(sys.argv[2])) # validate input, assume negative triforces are empty
+    if len(sys.argv) == 3:
+        sys.argv.append(TRIANGLE_CHAR)
+    print(f"printing a {int(sys.argv[1])}-force of scale {int(sys.argv[2])} made up of {sys.argv[3]}'s")
+    print_triangle(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3])
 except:
-    print("usage: triangle.py <level> <scale>")        # only way to mess up is bad usage
+    print("usage: triangle.py <level> <scale> [substance]")        # only way to mess up is bad usage
