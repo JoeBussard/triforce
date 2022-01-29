60142957020599beeb56a6a90a2248b13b629528
Joe Bussard <jbussard@ucsc.edu>
2022-01-29 13:21:18 -0800

    Update triangle.py
    added documentation

--- triangle.py
+++ triangle.py
@@ -3,15 +3,16 @@ import sys # for command line arguments
 TRIANGLE_CHAR = "▲"
 
 def print_triangle(height, size):
-    for level in range(height):
-        for x in range(size):
-            print(" " * (size * (height - level - 1)),
-                    (" " * (size - x - 1) +
-                    TRIANGLE_CHAR * (2*x + 1) +
-                    " " * (size - x)) * (level+1),
-                    sep='')
+    for level in range(height):                        # height of the n-force
+        for x in range(size):                          # scale of each triangle
+            print(" " * (size * (height - level - 1)), # left margin for this entire level
+                    (" " * (size - x - 1) +            # left margin before the first ▲
+                    TRIANGLE_CHAR * (2*x + 1) +        # the ▲'s
+                    " " * (size - x)) * (level+1),     # right margin after the last ▲
+                                                       # repeat for all triangles on this level
+                    sep='')                            # all spacing is handled manually
 try:
     print(f"printing a {int(sys.argv[1])}-force of scale {int(sys.argv[2])}")
-    print_triangle(int(sys.argv[1]), int(sys.argv[2]))
+    print_triangle(int(sys.argv[1]), int(sys.argv[2])) # validate input, assume negative triforces are empty
 except:
-    print("usage: triangle.py <level> <scale>")
+    print("usage: triangle.py <level> <scale>")        # only way to mess up is bad usage
